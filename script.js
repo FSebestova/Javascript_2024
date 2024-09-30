a4cfb5747f8a774acdc94dd0230b30ca60756252
Františka Šebestová <fsebestova@seznam.cz>
2024-09-30 20:05:09 +0200

    dodělaný úkol 1

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 var listek = 12
 var pocet_predstaveni_mesic = 15 
 var navstevniku = 175
+var student_listek = listek * 0.65
+var prijem_divadla_1 = (175 * 12) * 15
+var prijem_divadla_2 = ((navstevniku * 0.60) * listek) + ((navstevniku * 0.40) * student_listek)
 
-var prijem_divadla = (175 * 12) * 15
 
-document.body.innerHTML += "<p>" + "Celkova částka: "  + prijem_divadla  + "</p>"
+document.body.innerHTML += "<p>" + "Celkova částka: "  + prijem_divadla_1  + "</p>"
+
+document.body.innerHTML += "<p>" + "Celkova částka se slevou: "  + prijem_divadla_2 + "</p>"
+

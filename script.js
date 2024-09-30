3226a7124b8dc49d27524eecd34ac42f29afdc7a
Františka Šebestová <fsebestova@seznam.cz>
2024-09-30 19:50:42 +0200

    první úkol

---
+++ script.js
@@ -0,0 +1,7 @@
+var listek = 12
+var pocet_predstaveni_mesic = 15 
+var navstevniku = 175
+
+var prijem_divadla = (175 * 12) * 15
+
+document.body.innerHTML += "<p>" + "Celkova částka: "  + prijem_divadla  + "</p>"
